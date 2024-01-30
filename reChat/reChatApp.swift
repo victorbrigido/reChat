a02d19fbd60a98a5e683eebfcb361c8a7f5f346d
Victor Brigido <victorbrigido@icloud.com>
2024-01-30 10:51:13 -0300

    Initial Commit

---
+++ reChat/reChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  reChatApp.swift
+//  reChat
+//
+//  Created by Victor Brigido on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct reChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
